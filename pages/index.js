616437b410f735a89074ea4727c217ef097d6fca
frozenproof <trucling222@gmail.com>
2023-02-16 18:14:57 +0700

    Adding

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,29 @@ app.get("/outfit",(req,res)=>{
 		jeans: _.sample(jeans),
 		shoes: _.sample(shoes)
 	});   
-    
+	//Pass the variables off as json strings
+    res.json({
+		top: _.sample(tops),
+		jeans: _.sample(jeans),
+		shoes: _.sample(shoes)
+	});
     res.send("This is working as a server");
 })
+
+app.get("/comments/:id", async (req, res) => {
+	const id = req.params.id;
+	let content;
+
+	try {
+		content = await fs.readFile(`data/comments/${id}.txt`, "utf-8");
+	} catch (err) {
+		return res.sendStatus(404);
+	}
+
+	res.json({
+		content: content
+	});
+});
 
 app.listen(3000,()=>console.log("Server is currently running"));
 
