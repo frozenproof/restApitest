ba57f924890e750f076e0fa34cc922edcc266f74
frozenproof <trucling222@gmail.com>
2023-02-16 17:43:48 +0700

    Learning

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Forced repo
 # restApitest
 Test for NCKH and turning off debugging mode.
+
+
+Document resource:
+#1:https://www.youtube.com/watch?v=TcvOgwQPsSo
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+const fs = require("fs/promises");
+const express = require("express");
+const cors = require("cors");
+const _ = require("lodash");
+const { v4: uuid } = require("uuid");
 
+const app = express();
 
+app.get("/outfit",(req,res)=>{
+    const tops = ["Black", "White", "Orange", "Navy"];
+	const jeans = ["Grey", "Dark Grey", "Black", "Navy"];
+	const shoes = ["White", "Grey", "Black"];
 
+	res.json({
+		top: _.sample(tops),
+		jeans: _.sample(jeans),
+		shoes: _.sample(shoes)
+	});   
+    
+    res.send("This is working as a server");
+})
 
-console.log("Hello world")
+app.listen(3000,()=>console.log("Server is currently running"));
+
+// console.log("Hello world");
